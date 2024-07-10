51b37c367d41247d9a81fe6deed672dfc609abc4
matheusp5 <mxtheuzchika@gmail.com>
2024-07-10 16:37:15 -0300

    base entity

---
+++ src/core/entities/base-entity.ts
@@ -0,0 +1,17 @@
+export abstract class BaseEntity<Props> {
+    private _id: string
+    protected props: Props
+
+    public get id() {
+        return this._id
+    }
+
+    public equals(entity: BaseEntity<unknown>) {
+        return entity.id === this.id
+    }
+
+    protected constructor(id: string, props: Props) {
+        this.props = props
+        this._id = id
+    }
+}
