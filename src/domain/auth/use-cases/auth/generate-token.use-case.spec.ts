04e2691245c2f31df614dfc5f24f64e51466a343
matheusp5 <mxtheuzchika@gmail.com>
2024-07-12 19:26:35 -0300

    generate token use case tests

---
+++ src/domain/auth/use-cases/auth/generate-token.use-case.spec.ts
@@ -0,0 +1,25 @@
+import { JwtEncrypter } from "@/infra/cryptography/jwt-encrypter";
+import { IEncrypter } from "../../cryptography/encrypter";
+import { GenerateTokenUseCase } from "./generate-token.use-case";
+import { TokenType } from "@/core/enums/token-type";
+import moment from "moment";
+
+let sut: GenerateTokenUseCase;
+let encrypter: IEncrypter;
+
+describe("Generate Token Use Case", () => {
+    beforeAll(() => {
+        encrypter = new JwtEncrypter()
+        sut = new GenerateTokenUseCase(encrypter)
+    })
+
+    it('should generate a token', async () => {
+        const accountId = "teste"
+
+        const createTokenResponse = await sut.execute({ accountId, type: TokenType.ACCESS })
+
+        expect(createTokenResponse.token).toBeTruthy()
+        expect(typeof createTokenResponse.token).toBe("string")
+        expect(createTokenResponse.expiresAt > moment().toDate()).toBeTruthy()
+    })
+})
