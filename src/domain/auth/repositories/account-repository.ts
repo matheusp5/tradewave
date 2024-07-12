35c3f7abbb2f8530d1244e7ab89aeb32c31314fa
matheusp5 <mxtheuzchika@gmail.com>
2024-07-12 18:18:20 -0300

    create account use case

--- src/domain/auth/repositories/account-repository.ts
+++ src/domain/auth/repositories/account-repository.ts
@@ -4,6 +4,8 @@ import { Account } from "../entities/account";
 export interface IAccountRepository {
     getAll(): Promise<Account[]>
     getById(id: string): Promise<Account>
+    getByEmail(email: string): Promise<Account>
+    getByUsername(username: string): Promise<Account>
     create(account: ICreateAccountDTO): Promise<Account>
     deleteById(id: string): Promise<void>
     updateById(id: string, account: ICreateAccountDTO): Promise<Account>
