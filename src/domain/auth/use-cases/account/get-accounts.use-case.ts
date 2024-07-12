562325bae73204fadc53cf7f1fd748f44be7ad62
matheusp5 <mxtheuzchika@gmail.com>
2024-07-12 18:22:37 -0300

    get accounts use case

---
+++ src/domain/auth/use-cases/account/get-accounts.use-case.ts
@@ -0,0 +1,20 @@
+import { IUseCase } from "@/core/types/use-case";
+import { Account } from "../../entities/account";
+import { IAccountRepository } from "../../repositories/account-repository";
+
+interface IGetAccountsUseCaseResponse {
+    accounts: Account[]
+}
+
+export class GetAccountsUseCase implements IUseCase<{}, Promise<IGetAccountsUseCaseResponse>> {
+    constructor(
+        private accountRepository: IAccountRepository
+    ) { }
+
+    async execute(): Promise<IGetAccountsUseCaseResponse> {
+        const accounts = await this.accountRepository.getAll()
+
+        return { accounts }
+    }
+
+}
