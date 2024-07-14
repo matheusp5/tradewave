815eebf5e7aed58bebe56ddac5bedb9f5bca9427
matheusp5 <mxtheuzchika@gmail.com>
2024-07-14 01:26:18 -0300

    presenters

---
+++ src/infra/http/fastify/presenters/account.presenter.ts
@@ -0,0 +1,13 @@
+import { Account } from "@/domain/auth/entities/account";
+
+export class AccountPresenter {
+    static toHttp(account: Account) {
+        return {
+            id: account.id,
+            firstName: account.firstName,
+            lastName: account.lastName,
+            email: account.email,
+            createdAt: account.createdAt,
+        }
+    }
+}
