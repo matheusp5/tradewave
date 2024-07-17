76dd5e342405d6991287a5ab9a37a8ba7d945a0b
matheusp5 <mxtheuzchika@gmail.com>
2024-07-17 03:45:28 -0300

    connect to database

--- src/infra/env/index.ts
+++ src/infra/env/index.ts
@@ -6,7 +6,8 @@ const envVarsSchema = yup.object({
     .oneOf(['development', 'production', 'test'])
     .required('NODE_ENV é obrigatório'),
   PORT: yup.number().default(3333).required('PORT é obrigatório'),
-  JWT_SECRET: yup.string().required('JWT_SECRET é obrigatório')
+  JWT_SECRET: yup.string().required('JWT_SECRET é obrigatório'),
+  DATABASE_URL: yup.string().required('DATABASE_URL é obrigatório')
 })
 
 const envVars = envVarsSchema.validateSync(process.env)
@@ -14,6 +15,7 @@ const envVars = envVarsSchema.validateSync(process.env)
 export default {
   nodeEnv: envVars.NODE_ENV,
   port: envVars.PORT,
+  databaseUrl: envVars.DATABASE_URL,
   jwt: {
     secret: envVars.JWT_SECRET
   }
